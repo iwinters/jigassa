31433e620217ca786b648a4102a4b52298238b0d
Isaac Winters <isaacawinters@gmail.com>
2021-08-07 17:29:43 -0400

    stripe redirect urls must be absolute, not relative

--- members/views.py
+++ members/views.py
@@ -104,8 +104,8 @@ def checkout(request):
             }],
             mode='subscription',
             allow_promotion_codes=True,
-            success_url='http://{DOMAIN}/success?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url='http://{DOMAIN}/cancel',
+            success_url='http://tutorscourse.com/success?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url='http://tutorscourse.com/cancel',
         )
         return redirect(session.url, code=303)
         #return render(request, 'members/checkout.html', {'final_dollar': final_dollar, 'session_id': session.id})
